310fb40ff304d6f4ad281f57c99209f08f5b90ac
Tricia Ball <trish@ted.com>
2022-05-20 01:06:57 -0500

    Add initializer for Entry
    Entry objects will take in an entry_rows Array so it can be parsed into
    InputCharacters.

--- lib/bank_ocr/entry.rb
+++ lib/bank_ocr/entry.rb
@@ -3,6 +3,14 @@
 module BankOCR
   # This class represents a group of input characters
   class Entry
+    attr_reader :entry_rows
+
+    def initialize(entry_rows)
+      raise ArgumentError, 'entry_rows must be an Array' unless entry_rows.is_a?(Array)
+
+      @entry_rows = entry_rows
+    end
+
     def valid?
       false
     end
--- spec/lib/bank_ocr/entry_spec.rb
+++ spec/lib/bank_ocr/entry_spec.rb
@@ -1,6 +1,35 @@
 # frozen_string_literal: true
 
 describe BankOCR::Entry do
+  let(:entry_rows) do
+    [
+      '    _  _     _  _  _  _  _ ',
+      '  | _| _||_||_ |_   ||_||_|',
+      '  ||_  _|  | _||_|  ||_| _|'
+    ]
+  end
+
+  it 'takes in rows for an entry' do
+    entry = described_class.new(entry_rows)
+
+    expect(entry).to be_a(BankOCR::Entry)
+    expect(entry.entry_rows).to eq(entry_rows)
+  end
+
+  it 'raises an error if no entry_rows is provided' do
+    entry_rows = nil
+
+    expect { described_class.new(entry_rows) }
+      .to raise_error(BankOCR::ArgumentError, 'entry_rows must be an Array')
+  end
+
+  it 'raises an error if entry_rows is not an Array' do
+    entry_rows = 'character matrix'
+
+    expect { described_class.new(entry_rows) }
+      .to raise_error(BankOCR::ArgumentError, 'entry_rows must be an Array')
+  end
+
   describe 'valid?' do
     it 'consists of 9 input characters'
     it 'has an empty last line'
