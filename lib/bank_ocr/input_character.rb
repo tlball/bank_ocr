b8dfe3b41eacabef4fc36635b3eda0f6ac80558b
Tricia Ball <trish@ted.com>
2022-05-19 23:23:26 -0500

    Initialize InputCharacter
    Add initialize for InputCharacter which takes in a character matrix, and
    handles unexpected parameters.

--- lib/bank_ocr/input_character.rb
+++ lib/bank_ocr/input_character.rb
@@ -4,8 +4,18 @@ module BankOCR
   # This class represents characters read in from the input file and ensures they
   # are valid
   class InputCharacter
+    attr_reader :character_matrix
+
+    def initialize(character_matrix)
+      raise ArgumentError, 'character_matrix must be an Array' unless character_matrix.is_a?(Array)
+
+      @character_matrix = character_matrix
+    end
+
     def valid?
       false
     end
+
+    class ArgumentError < ::ArgumentError; end
   end
 end
--- spec/lib/bank_ocr/input_character_spec.rb
+++ spec/lib/bank_ocr/input_character_spec.rb
@@ -1,6 +1,33 @@
 # frozen_string_literal: true
 
 describe BankOCR::InputCharacter do
+  it 'takes in a character matrix on creation' do
+    character_matrix = [
+      [' ', '_', ' '],
+      ['|', '_', '|'],
+      [' ', '_', '|']
+    ]
+
+    input_character = described_class.new(character_matrix)
+
+    expect(input_character).to be_a(BankOCR::InputCharacter)
+    expect(input_character.character_matrix).to eq(character_matrix)
+  end
+
+  it 'raises an error if no character_matrix is provided' do
+    character_matrix = nil
+
+    expect { described_class.new(character_matrix) }
+      .to raise_error(BankOCR::InputCharacter::ArgumentError, 'character_matrix must be an Array')
+  end
+
+  it 'raises an error if character_matrix is not an Array' do
+    character_matrix = 'character matrix'
+
+    expect { described_class.new(character_matrix) }
+      .to raise_error(BankOCR::InputCharacter::ArgumentError, 'character_matrix must be an Array')
+  end
+
   describe 'valid?' do
     it 'has a width of 3'
     it 'has a height of 3'
